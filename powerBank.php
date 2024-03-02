69704570707cdab92d92b012b8219774f1817ca6
MichaelAnthonyRodriguez <mr94@njit.edu>
2024-03-02 06:39:38 -0500

    Phase 2

---
+++ powerBank.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 0.2b
+ */
+
+//
+// Database `mr94`
+//
+
+// `mr94`.`powerBank`
+$powerBank = array(
+  array('powerBankID' => '1','powerBankCategoryID' => '1','powerBankCode' => 'cap_1','powerBankName' => 'Anker XL Power Bank','description' => 'This power bank is designed for large capacity','mah' => '27000','price' => '67.99','dateCreated' => '2024-03-01 22:24:17'),
+  array('powerBankID' => '2','powerBankCategoryID' => '1','powerBankCode' => 'cap_2','powerBankName' => 'Samsung Large Power Bank','description' => 'This power bank is designed for large capacity','mah' => '20000','price' => '79.99','dateCreated' => '2024-03-01 22:25:09'),
+  array('powerBankID' => '3','powerBankCategoryID' => '1','powerBankCode' => 'cap_3','powerBankName' => 'Apple Max Power Bank','description' => 'This power bank is designed for the largest capacity','mah' => '70000','price' => '267.99','dateCreated' => '2024-03-01 22:26:11'),
+  array('powerBankID' => '4','powerBankCategoryID' => '1','powerBankCode' => 'cap_4','powerBankName' => 'UGREEN Max Power Bank','description' => 'This power bank is designed for large capacity','mah' => '21000','price' => '37.99','dateCreated' => '2024-03-01 22:27:07'),
+  array('powerBankID' => '5','powerBankCategoryID' => '2','powerBankCode' => 'sol_1','powerBankName' => 'Anker Solar Power Bank','description' => 'This power bank is designed for solar charging','mah' => '10000','price' => '60.99','dateCreated' => '2024-03-01 22:56:29'),
+  array('powerBankID' => '6','powerBankCategoryID' => '2','powerBankCode' => 'sol_2','powerBankName' => 'UGREEN Solar Power Bank','description' => 'This power bank is designed for solar charging','mah' => '15000','price' => '75.99','dateCreated' => '2024-03-01 22:57:07'),
+  array('powerBankID' => '7','powerBankCategoryID' => '2','powerBankCode' => 'sol_3','powerBankName' => 'Anker Solar Power Bank','description' => 'This power bank is designed for solar charging','mah' => '20000','price' => '97.99','dateCreated' => '2024-03-01 22:57:42'),
+  array('powerBankID' => '8','powerBankCategoryID' => '2','powerBankCode' => 'sol_4','powerBankName' => 'Samsung Solar Power Bank','description' => 'This power bank is designed for solar charging','mah' => '27000','price' => '149.99','dateCreated' => '2024-03-01 22:58:16'),
+  array('powerBankID' => '9','powerBankCategoryID' => '3','powerBankCode' => 'slim_1','powerBankName' => 'Samsung compact Power Bank','description' => 'This power bank is designed for portability','mah' => '5000','price' => '19.99','dateCreated' => '2024-03-01 22:59:44'),
+  array('powerBankID' => '10','powerBankCategoryID' => '3','powerBankCode' => 'slim_2','powerBankName' => 'Anker compact Power Bank','description' => 'This power bank is designed for portability','mah' => '8000','price' => '29.99','dateCreated' => '2024-03-01 23:01:17'),
+  array('powerBankID' => '11','powerBankCategoryID' => '3','powerBankCode' => 'slim_3','powerBankName' => 'Apple compact Power Bank','description' => 'This power bank is designed for portability','mah' => '10000','price' => '49.99','dateCreated' => '2024-03-01 23:01:48'),
+  array('powerBankID' => '12','powerBankCategoryID' => '3','powerBankCode' => 'slim_4','powerBankName' => 'UGREEN compact Power Bank','description' => 'This power bank is designed for portability','mah' => '9000','price' => '59.99','dateCreated' => '2024-03-01 23:02:23'),
+  array('powerBankID' => '13','powerBankCategoryID' => '4','powerBankCode' => 'fast_1','powerBankName' => 'UGREEN 100W Power Bank','description' => 'This power bank is designed for speed','mah' => '12000','price' => '69.99','dateCreated' => '2024-03-01 23:08:35'),
+  array('powerBankID' => '14','powerBankCategoryID' => '4','powerBankCode' => 'fast_2','powerBankName' => 'Samsung 100W Power Bank','description' => 'This power bank is designed for speed','mah' => '15000','price' => '89.99','dateCreated' => '2024-03-01 23:09:05'),
+  array('powerBankID' => '15','powerBankCategoryID' => '4','powerBankCode' => 'fast_3','powerBankName' => 'Apple 100W Power Bank','description' => 'This power bank is designed for speed','mah' => '20000','price' => '129.99','dateCreated' => '2024-03-01 23:09:43'),
+  array('powerBankID' => '16','powerBankCategoryID' => '4','powerBankCode' => 'fast_4','powerBankName' => 'Anker 200W Power Bank','description' => 'This power bank is designed for speed','mah' => '27000','price' => '150.99','dateCreated' => '2024-03-01 23:10:37'),
+  array('powerBankID' => '17','powerBankCategoryID' => '5','powerBankCode' => 'wire_2','powerBankName' => 'Anker wireless Power Bank','description' => 'This power bank is designed for wireless charging','mah' => '8000','price' => '21.99','dateCreated' => '2024-03-01 23:12:20'),
+  array('powerBankID' => '18','powerBankCategoryID' => '5','powerBankCode' => 'wire_3','powerBankName' => 'Samsung wireless Power Bank','description' => 'This power bank is designed for wireless charging','mah' => '10000','price' => '35.99','dateCreated' => '2024-03-01 23:19:04'),
+  array('powerBankID' => '19','powerBankCategoryID' => '5','powerBankCode' => 'wire_1','powerBankName' => 'Anker wireless Power Bank','description' => 'This power bank is designed for all wireless charging iphones','mah' => '10000','price' => '35.99','dateCreated' => '2024-03-01 22:21:41'),
+  array('powerBankID' => '20','powerBankCategoryID' => '5','powerBankCode' => 'wire_4','powerBankName' => 'UGREEN wireless Power Bank','description' => 'This power bank is designed for wireless charging','mah' => '20000','price' => '55.99','dateCreated' => '2024-03-01 23:19:39')
+);
+
+// `mr94`.`powerBankCategories`
+$powerBankCategories = array(
+  array('powerBankCategoryID' => '1','powerBankCategoryName' => 'Capacity','dateCreated' => '2024-03-01 17:27:01'),
+  array('powerBankCategoryID' => '2','powerBankCategoryName' => 'Solar','dateCreated' => '2024-03-01 17:27:01'),
+  array('powerBankCategoryID' => '3','powerBankCategoryName' => 'Slim','dateCreated' => '2024-03-01 17:27:01'),
+  array('powerBankCategoryID' => '4','powerBankCategoryName' => 'Fast','dateCreated' => '2024-03-01 17:27:01'),
+  array('powerBankCategoryID' => '5','powerBankCategoryName' => 'Wireless','dateCreated' => '2024-03-01 17:27:01')
+);
