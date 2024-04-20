80d54ccb6ff11bf5d4a4f12ea6c0f8fcd8fc26f6
MichaelAnthonyRodriguez <mr94@njit.edu>
2024-04-19 23:38:25 -0400

    fix

--- database_local.php
+++ database_local.php
@@ -2,12 +2,12 @@
   // MODIFY LINE 3
   function getDB() {
     // Chapter 4 Slide 24
-    $dsn = 'mysql:host=localhost;port=3306;dbname=mr94';
-    $username = 'root';
-    $password = '';
-    // $dsn = 'mysql:host=sql1.njit.edu;port=3306;dbname=mr94';
-    // $username = 'mr94';
-    // $password = 'Mikorod721?';
+    // $dsn = 'mysql:host=localhost;port=3306;dbname=mr94';
+    // $username = 'root';
+    // $password = '';
+    $dsn = 'mysql:host=sql1.njit.edu;port=3306;dbname=mr94';
+    $username = 'mr94';
+    $password = 'Mikorod721?';
 
     try {
       $db = new PDO($dsn, $username, $password);
