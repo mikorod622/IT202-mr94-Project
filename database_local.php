20add66a73086f351b370eedba7672cba1d2bdff
mikorod622 <mikorod622@gmail.com>
2025-02-20 18:16:28 -0500

    db

--- database_local.php
+++ database_local.php
@@ -2,7 +2,7 @@
   // MODIFY LINE 3
   function getDB() {
     // Updated database connection details
-    $dsn = 'mysql:host=13855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;port=3306;dbname=wckauxxx5t8yb9p5';
+    $dsn = 'mysql://wj5myz4787qfnjp9:g07pg3to9f6hbx46@l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/wckauxxx5t8yb9p5';
     $username = 'wj5myz4787qfnjp9';
     $password = 'g07pg3to9f6hbx46';
 
