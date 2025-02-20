0711481ca7e06598cb2e8560b5ba377278569988
mikorod622 <mikorod622@gmail.com>
2025-02-20 13:03:10 -0500

    db fix

--- database_local.php
+++ database_local.php
@@ -2,7 +2,7 @@
   // MODIFY LINE 3
   function getDB() {
     // Chapter 4 Slide 24
-    $dsn = 'mysql:host=us-east-1.rds.amazonaws.com;port=3306;dbname=heroku_wckauxxx5t8yb9p5';
+    $dsn = 'mysql:host=us-east-1.rds.amazonaws.com;port=3306;dbname=wckauxxx5t8yb9p5';
     $username = 'wj5myz4787qfnjp9';
     $password = 'g07pg3to9f6hbx46';
 
