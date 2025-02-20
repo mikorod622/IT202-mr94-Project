d3060ac73d1bdd8c46d0b1cf44dacaaa75f09a5b
mikorod622 <mikorod622@gmail.com>
2025-02-20 13:07:29 -0500

    db fix

--- database_local.php
+++ database_local.php
@@ -1,19 +1,20 @@
 <?php
   // MODIFY LINE 3
   function getDB() {
-    // Chapter 4 Slide 24
-    $dsn = 'mysql:host=us-east-1.rds.amazonaws.com;port=3306;dbname=wckauxxx5t8yb9p5';
+    // Updated database connection details
+    $dsn = 'mysql:host=13855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;port=3306;dbname=wckauxxx5t8yb9p5';
     $username = 'wj5myz4787qfnjp9';
     $password = 'g07pg3to9f6hbx46';
 
     try {
       $db = new PDO($dsn, $username, $password);
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $ex) {
       $error_message = $ex->getMessage();
       include('database_error.php');
       exit();
     }
-  // MODIFY LINE 18 and 19
-  return $db;
+    // MODIFY LINE 18 and 19
+    return $db;
   }
 ?>
