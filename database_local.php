f168c42e86e967405958ef2e95a6ccefba7b89da
mikorod622 <mikorod622@gmail.com>
2025-02-20 19:00:21 -0500

    db

--- database_local.php
+++ database_local.php
@@ -2,19 +2,28 @@
   // MODIFY LINE 3
   function getDB() {
     // Updated database connection details
-    $dsn = 'mysql://wj5myz4787qfnjp9:g07pg3to9f6hbx46@l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/wckauxxx5t8yb9p5';
-    $username = 'wj5myz4787qfnjp9';
-    $password = 'g07pg3to9f6hbx46';
+    $url = getenv('JAWSDB_URL');
+    $dbparts = parse_url($url);
+    
+    $hostname = $dbparts['l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	'];
+    $username = $dbparts['wj5myz4787qfnjp9'];
+    $password = $dbparts['g07pg3to9f6hbx46'];
+    $database = ltrim($dbparts['wckauxxx5t8yb9p5'],'/');
 
     try {
-      $db = new PDO($dsn, $username, $password);
+      $db = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+      // set the PDO error mode to exception
       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch(PDOException $ex) {
-      $error_message = $ex->getMessage();
-      include('database_error.php');
-      exit();
-    }
+      echo "Connected successfully";
+      }
+    catch(PDOException $ex)
+      {
+        $error_message = $ex->getMessage();
+        include('database_error.php');
+        exit();
+      }
     // MODIFY LINE 18 and 19
     return $db;
   }
+
 ?>
